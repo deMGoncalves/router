8afb9c40744a1b952309ff5c8ebf7c2d7a282ed6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-02-23 16:22:06 -0300

    build(dist): automatic new publish

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,2 @@
-"use strict";
+export { default } from "./router";
+export { default as urlFor } from "./urlfor";
---
+++ dist/urlfor.js
@@ -0,0 +1,3 @@
+import * as f from 'f';
+import urls from "./urls";
+export default (name, ...args) => f.replace(f.or(urls[name], ''), /(\(\[[az09\-_]+\]\+?\*?\))/g, f.call(f.shift, args));
